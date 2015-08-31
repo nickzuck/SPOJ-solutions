0ae32af024f9cbfe99214025d45d0daaf570ece9
nickzuck <nikhil1945.singh@gmail.com>
2015-08-31 11:43:49 +0530

    Must give a correct answer..(I guess its the minor bugs)

--- acode.cpp
+++ acode.cpp
@@ -1,52 +1,47 @@
-#include<cstdio>
-#include<string.h>
-#include<cmath>
-
+#include<iostream>
+#include<algorithm>
+#include<cstring>
 #define MAX 5002
 
-//using namespace std ; 
+using namespace std ; 
 
-int main (){
-    char str[MAX] ;
-    long long int dp[MAX] , i , len  ; 
-    while(1) {
-        scanf("%s",str) ; 
-	if (*str == '0') 
-	    break ; 
-	len = strlen(str) ;  //getting the length of the string 
 
-	//setting the dp parameters 
-	dp[-1] = 1 ; 
-	/*for (i = 2 ;i<len ; i++){  dp[i] =0 ;} */
-	dp[0] = 1  ; 
+int main() 
+{
 
-	
-	for (i = 1 ;i<len ; i++){
-	    long long int a = (str[i-1] -'0')*10 + str[i]-'0' ;
-	   /* if (!strcmp(str , "502")){printf ("str[i]= %d and a = %d\n", str[i] , a ) ; }*/
-	    
-	    if (str[i]!='0' && str[i-1]!='0' && str[i+1]!='0' &&  a <27)
-	    {
-		dp[i] = dp[i-1] + dp[i-2] ; 
-		if (!strcmp(str , "502")){
-		    printf ("in last else part\n") ; 
+	char str[MAX]; 
+	int temp , dp[MAX] = {0}, length; 
+	while (1){
+		cin >> str ; 
+		if (str[0]== '0')
+			break ;
+
+		//setting up dp parameters 
+		dp[0]= 1 ; 
+		dp[-1] = 1 ;  
+		length = strlen(str);
+		cout << str << endl ;
+		for (int i = 1 ;i<length ; i++ ){
+			temp = (str[i-1]-'0'); 
+			//cout << "Temp at first stage = " << temp << endl;
+			temp *= 10 ; 
+			//cout << "Temp at second stage = " << temp << endl ;
+			temp += str[i]-'0' ;  
+
+			cout << "Temp = "<< temp << endl ;
+			dp[-1] = 1 ; 
+			if (str[i]- '0' > 0 ){ //valid single digit number
+				cout << "i and  i-1 = " << i << " " << i-1 << endl ;
+				cout << "dp[i] && dp[i-1] = " << dp[i] << "  "<< dp[i-1] << endl ; 
+				dp[i] = dp[i-1] ; 
+				if (temp > 0 && temp <27){ // for valid 2 digit number
+					cout << "i and  i-2 = " << i <<" "<< i-2<< endl ;
+					cout << "dp[i] && dp[i-2] = " << dp[i] << "  "<< dp[i-2] << endl ; 
+					dp[i] += dp[i-2] ;
+				}
+			}		
 		}
-	    }
-	    else if (a>=27 && str[i] == '0'){
- 		dp[len-1] = 0 ; 
-		i= len ; 
-		//printf ("We are in invalid case \n")  ;  
-	    }
-	    else {
-		dp[i] = dp[i-1] ; 
-		/*if (!strcmp(str , "502")){
-		    printf ("in last else part\n"); 
-		}*/
-	    }
+		cout << dp[length-1]<< endl; 
 	}
-	    
-	 
-	printf("%lld\n",dp[len-1]) ; 
-    }
-return 0 ; 
+return 0 ;
 }
