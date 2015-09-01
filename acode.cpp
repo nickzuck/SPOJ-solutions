36acc227b61791255d7fd1b26acc3a17963da8f2
nickzuck <nikhil1945.singh@gmail.com>
2015-09-01 11:17:11 +0530

    Getting correct answers on sample test cases but WA on SPOJ

--- acode.cpp
+++ acode.cpp
@@ -1,47 +1,26 @@
-#include<iostream>
-#include<algorithm>
+#include<cstdio>
 #include<cstring>
-#define MAX 5002
 
-using namespace std ; 
-
-
-int main() 
+char input[5002]= "";
+unsigned long long dp[5002];
+int main ()
 {
-
-	char str[MAX]; 
-	int temp , dp[MAX] = {0}, length; 
-	while (1){
-		cin >> str ; 
-		if (str[0]== '0')
-			break ;
-
-		//setting up dp parameters 
-		dp[0]= 1 ; 
-		dp[-1] = 1 ;  
-		length = strlen(str);
-		cout << str << endl ;
-		for (int i = 1 ;i<length ; i++ ){
-			temp = (str[i-1]-'0'); 
-			//cout << "Temp at first stage = " << temp << endl;
-			temp *= 10 ; 
-			//cout << "Temp at second stage = " << temp << endl ;
-			temp += str[i]-'0' ;  
-
-			cout << "Temp = "<< temp << endl ;
-			dp[-1] = 1 ; 
-			if (str[i]- '0' > 0 ){ //valid single digit number
-				cout << "i and  i-1 = " << i << " " << i-1 << endl ;
-				cout << "dp[i] && dp[i-1] = " << dp[i] << "  "<< dp[i-1] << endl ; 
-				dp[i] = dp[i-1] ; 
-				if (temp > 0 && temp <27){ // for valid 2 digit number
-					cout << "i and  i-2 = " << i <<" "<< i-2<< endl ;
-					cout << "dp[i] && dp[i-2] = " << dp[i] << "  "<< dp[i-2] << endl ; 
-					dp[i] += dp[i-2] ;
-				}
-			}		
-		}
-		cout << dp[length-1]<< endl; 
-	}
-return 0 ;
+    scanf ("%s",input) ; 
+    while(strcmp(input , "0")){
+        int len = strlen(input); 
+        dp[0]= 1 ; 
+        for (int i = 1; i<len ; i++){
+            int temp = 0 ; 
+            temp = (input[i-1]-'0') * 10 ;
+            temp += (input[i]- '0') ;
+            if (input[i] - '0'){
+                dp[i]= dp[i-1] ;
+            }
+            if (temp <=26 && temp > 9){
+                dp[i] += dp[i-2 < 0 ?0:i-2] ;
+            }
+        }
+        printf ("%llu\n",dp[len-1]) ;
+        scanf ("%s",input) ; 
+    }
 }
