a050541ac1d175b0295c0d2f8420d43318e74f23
nickzuck <nikhil1945.singh@gmail.com>
2016-09-14 11:26:28 +0530

    Still WA...Handled the case for no combinations

--- acode.cpp
+++ acode.cpp
@@ -8,11 +8,18 @@ int main ()
     scanf ("%s",input) ; 
     while(strcmp(input , "0")){
         int len = strlen(input); 
+        bool error = false ;
         dp[0]= 1 ; 
         for (int i = 1; i<len ; i++){
             int temp = 0 ; 
             temp = (input[i-1]-'0') * 10 ;
             temp += (input[i]- '0') ;
+        
+            if(input[i] == '0' && (temp > 26 || temp == 0)){
+                //printf("%c %d\n" , input[i], temp);
+                error = true ; 
+                break ;
+            }
             if (input[i] - '0'){
                 dp[i]= dp[i-1] ;
             }
@@ -20,7 +27,14 @@ int main ()
                 dp[i] += dp[i-2 < 0 ?0:i-2] ;
             }
         }
-        printf ("%llu\n",dp[len-1]) ;
+        
+                //printf("Input was %s\n", input);
+        if(error){
+            printf("0\n");
+        }
+        else{
+            printf ("%llu\n",dp[len-1]) ;
+        }
         scanf ("%s",input) ; 
     }
 }
