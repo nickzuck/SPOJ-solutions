cc7d98f6feaa803f51288f041b41ac72a3ab1acd
nickzuck <nikhil1945.singh@gmail.com>
2016-09-14 12:12:08 +0530

    Finally AC.. Garbage values kills

--- acode.cpp
+++ acode.cpp
@@ -2,15 +2,17 @@
 #include<cstring>
 
 char input[5002]= "";
-unsigned long long dp[5002];
 int main ()
 {
     scanf ("%s",input) ; 
     while(strcmp(input , "0")){
+        
+        unsigned long long dp[5002]= {0};
         int len = strlen(input); 
         bool error = false ;
         dp[0]= 1 ; 
         for (int i = 1; i<len ; i++){
+            //printf("dp[i] === %lld, i === %d\n", dp[i], i);
             int temp = 0 ; 
             temp = (input[i-1]-'0') * 10 ;
             temp += (input[i]- '0') ;
@@ -23,9 +25,15 @@ int main ()
             if (input[i] - '0'){
                 dp[i]= dp[i-1] ;
             }
+            //printf("First if dp[i] = %lld vale = %c\n", dp[i], input[i]);
+            ///----Add condition here for input[i] == 0
+            
             if (temp <=26 && temp > 9){
                 dp[i] += dp[i-2 < 0 ?0:i-2] ;
             }
+            //printf("Second if dp[i] = %lld\n", dp[i]);
+
+
         }
         
                 //printf("Input was %s\n", input);
