97f3b444dfd5a245ca55ecd362ab5d40bdc4fa58
nickzuck <nikhil1945.singh@gmail.com>
2016-09-19 22:39:36 +0530

    Still SIGSEGV

--- gss1.cpp
+++ gss1.cpp
@@ -1,27 +1,28 @@
 #include<iostream>
 #include<algorithm>
 #include<cstring> //For memset
+#include<climits>
+#include<cmath>
 
-#define MAX  101
-#define inf 15008
+#define MAX 402 
+#define inf LONG_MAX
 using namespace std ;
 
-int arr[MAX];
-int tree[2*MAX + 1];
+long long arr[MAX];
 
 //function definition
-//void update(int node, int a, int b, int i, int j , int value);
-int query(int node , int a , int b , int i , int j);
+//void update(long long node, long long a, long long b, long long i, long long j , long long value);
+long long query(long long node , long long a , long long b , long long i , long long j, long long []);
 
-void build_tree(int node, int a, int b);
+void build_tree(long long node, long long a, long long b, long long []);
 
 //function to find the middle value
-inline int getMid (int a , int b){
+inline long long getMid (long long a , long long b){
     return (a+b)/2;
 }
 
 //Find the maximum of three numbers
-inline int max(int a , int b , int c){
+inline long long max(long long a , long long b , long long c){
     if (a > b){
         if(a > c)
             return a ;
@@ -37,7 +38,7 @@ inline int max(int a , int b , int c){
 }
 
 //Build segment tree
-void build_tree(int node , int a , int b){
+void build_tree(long long node , long long a , long long b, long long tree[]){
     if (a > b){
         return ; // out of range
     }
@@ -45,35 +46,35 @@ void build_tree(int node , int a , int b){
         tree[node] = arr[a];
         return ;
     }
-    int mid = getMid(a , b);
-    build_tree(node*2 , a , mid);
-    build_tree(node*2+1 , mid+1 , b );
+    long long mid = getMid(a , b);
+    build_tree(node*2 , a , mid, tree);
+    build_tree(node*2+1 , mid+1 , b, tree);
     tree[node] = (tree[node*2] + tree[node*2 + 1]);
 }
 
-int queryUtil(int node , int i , int j){
+long long queryUtil(long long node , long long i , long long j, long long tree [] ){
     if(i == j){ //Leaf node
         return tree[node];
     }
     else{ //Other nodes
-        int mid = getMid( i , j);
-        int result = max(queryUtil(node*2 , i , mid) , queryUtil(node*2 + 1 ,mid+1 ,j) , tree[node]);
+        long long mid = getMid( i , j);
+        long long result = max(queryUtil(node*2 , i , mid, tree) , queryUtil(node*2 + 1 ,mid+1 ,j, tree) , tree[node]);
     }
 }
 
 //Query tree
-int query(int node , int a , int b , int i , int j ){
+long long query(long long node , long long a , long long b , long long i , long long j, long long tree []){
     if(a > b || a > j || b < i)
         return -inf; // Out of range
 
     if(a >= i && b <= j) // Current segment is totally within range [i, j]
-        return queryUtil(node , i , j); 
+        return queryUtil(node , i , j, tree); 
 
-    int mid = getMid(a , b);
-    int q1 = query(node*2, a, mid, i, j); // Query left child
-    int q2 = query(1+node*2, mid + 1, b, i, j); // Query right child
+    long long mid = getMid(a , b);
+    long long q1 = query(node*2, a, mid, i, j, tree); // Query left child
+    long long q2 = query(1+node*2, mid + 1, b, i, j, tree); // Query right child
 
-    int res = max(q1 , q2);// Return final result
+    long long res = max(q1 , q2);// Return final result
     
     return res;
 }
@@ -81,7 +82,9 @@ int query(int node , int a , int b , int i , int j ){
 //Driver Program
 int main()
 {
-    int n ,i , l , r , m ; 
+    long long n ,i , l , r , m ; 
+    long long *tree;
+    long long h  ;
     //cout << "Enter the number of elements in the array\t";
     cin >> n ;
 
@@ -89,14 +92,16 @@ int main()
     for(i = 1 ;i<=n;i++){
         cin >> arr[i];
     }
+    h = ceil(log2(n));
+    tree = new long long [ (long long) ( pow (2,  h+ 1) ) ];
     //initializing the array
     memset(tree , 0, sizeof(tree));
     //Build Tree
-    build_tree(1 , 1, n);
+    build_tree(1 , 1, n, tree);
     cin >> m ;
     while(m--){
         cin >> l >> r ;
-        cout << query(1 , 1 , n , l , r) << endl;
+        cout << query(1 , 1 , n , l , r, tree) << endl;
     }
 return 0 ;
 }
