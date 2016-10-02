0d821bad8721d44538055b32b863e8dcf4af873e
nickzuck <nikhil1945.singh@gmail.com>
2016-10-02 11:27:26 +0530

    AC .. Thanks to ravi kumar(ravi0213) for explaining me this.

--- arraysub.cpp
+++ arraysub.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<deque>
 
 using namespace std ;
 
@@ -23,16 +24,31 @@ int main()
     cin >> k ;
 
     // Processing
-    // Store the sum of first k elements
-    m = 0 ;
+    deque<int> window ; 
+    // Add first k elements to the queue
     for(i = 0 ; i < k ; i++){
-        m = max(v[i], m);
+        // Remove the previous smaller elements from the queue
+        while( !window.empty() && v[i] >= v[window.back()]){
+            window.pop_back() ;
+        }
+
+        window.push_back(i);
     }
-    print(m);
+    
+        print(v[window.front()]);
     // Use Sliding window algo to find the next n-k maxs
     for(;i < n; i++){
-        m = max(v[i], m);
-        print(m);
+        
+        while(!window.empty() && window.front() <= i-k){
+            window.pop_front();
+        }
+
+        while( !window.empty() && v[i] >= v[window.back()]){
+            window.pop_back() ;
+        }
+
+        window.push_back(i) ;
+        print(v[window.front()]);
     }
 return 0 ;
 }
