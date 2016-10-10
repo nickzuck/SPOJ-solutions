8fcb8c6f3ba3b952cc4018e92a345fab3a20ee6b
nickzuck <nikhil1945.singh@gmail.com>
2016-10-10 19:51:51 +0530

    AC .. Without space optimization this time, previously was messing up with the inputs, while loop for test cases when there isn't any while loop needed

--- ioipalin.cpp
+++ ioipalin.cpp
@@ -5,32 +5,33 @@
 #define MAX 6102
 using namespace std ;
 
-int dp[2][MAX+1];
+int dp[MAX+1][MAX+1];
 int main()
 {
     char str[MAX];
     int n, t  ; 
-    cin >> n; 
+        cin >> n ;
         cin >> str ;
         n = strlen(str) ;
-        for(int j = 0 ; j< n ; j++){
-            dp[0][j] = 0 ;
-            dp[1][j] = 0 ;
+        for(int i = 0 ; i< n ; i++){
+            dp[i][0] = 0 ;
+            dp[0][i] = 0 ;
         }
 
         // Processing
         for(int i= 1; i <= n ;i ++){
             for(int j = 1 ; j <= n ; j++){
                 if(str[i-1] == str[n-j]){
-                    dp[i%2][j] = dp[(i-1)%2][j-1] + 1;
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 }
                 else{
-                    dp[i%2][j] = max(dp[(i-1)%2][j], dp[i%2][j-1]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
         //cout << " n = " << n << "  dp[i][j] = " << dp[n][n] << endl; 
-        cout << (n - dp[1][n]) << endl ;
+        cout << (n - dp[n][n]) << endl ;
+    
     
 return 0 ;
 }
