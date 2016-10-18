fd07316d33dbd6709477774d9133d2a4bb8d341d
nickzuck <nikhil1945.singh@gmail.com>
2016-10-18 20:51:23 +0530

    AC . This one goes to Biran Bi...used monotone chain convex hull algorithm

--- bsheep.cpp
+++ bsheep.cpp
@@ -26,7 +26,7 @@ class Point{
 };
 
 bool compare(const Point &a, const Point &b){
-	return a.x == b.x ?(a.y == b.y ? a.pos < b.pos : a.y < b.y): a.x < b.x ;
+	return a.y == b.y ? (a.x == b.x ? a.pos < b.pos : a.x < b.x ): a.y < b.y;
 }
 
 double cross(const Point &O, const Point &A, const Point &B){
@@ -42,20 +42,28 @@ void print(vector<Point> v){
 }
 
 vector<Point> convex_hull(vector<Point> p, int n){
-	vector<Point> H(2*n);
-	int k = 0;
 
-	// Sort Points lexicographically
-	// print(p);
 	sort(p.begin(), p.end(), compare);
+	// cout << "After sorting\n";
 	// print(p);
 
-	// Build lower hull
+	int k = 1;
+    for(int i = 1 ; i < p.size() ; i++)
+        if(p[i-1] != p[i])
+            p[k++] = p[i];
+ 	p.resize(k);
+ 	n = k ;
+ 	// print(p);
+ 	vector<Point> H(2*n);
+	k = 0;
+
+	
 	for (int i = 0; i < n; ++i) {
 		while (k >= 2 && cross(H[k-2], H[k-1], p[i]) <= 0)
 			k--;
 		H[k++] = p[i];
 	}
+
 
 	// Build upper hull
 	for (int i = n-2, t = k+1; i >= 0; i--) {
@@ -64,16 +72,18 @@ vector<Point> convex_hull(vector<Point> p, int n){
 		H[k++] = p[i];
 	}
 
+	// cout << "upper hull completed\n" << endl ;
 	H.resize(k-1);
-	cout << "Print final answer\n";
-	print(H);
+	// cout << "Print final answer\n";
+	// print(H);
+	// cout << "Returning\n" ;
 	return H;
 }
 
 long long distanceSquare(Point p1 , Point p2){
 	long long dx = (p2.x - p1.x);
 	long long dy = (p2.y - p1.y);
-	cout << dx << " "  << dy << endl ;
+	// cout << dx << " "  << dy << endl ;
 	return (dx*dx + dy*dy);
 }
 
@@ -81,7 +91,7 @@ double hull_perimeter(vector<Point> v, int n){
 	double ans = 0;
 	for(int i = 0 ; i < n-1; i++){
 		ans += sqrt(distanceSquare(v[i], v[i+1]));
-		cout << "Distance till now = " << ans << endl;
+		// cout << "Distance till now = " << ans << endl;
 	}
 	ans += sqrt(distanceSquare(v[n-1], v[0]));
 	// cout << "distanceSquare = "<< ans << endl;
@@ -91,30 +101,40 @@ double hull_perimeter(vector<Point> v, int n){
 int main()
 {
 	int x , y ; 
-	vector<Point> points;
+	
 	int t, n ;
-	cin>> n ;
-	for(int i = 0 ;i <n ;i++){
-		cin >> x >> y ;
-		points.push_back(Point(x, y, i));
+	cin >> t ;
+	while(t--){
+		vector<Point> points;
+		cin>> n ;
+		// cout << "n = " << n << endl;
+		for(int i = 0 ;i <n ;i++){
+			cin >> x >> y ;
+			points.push_back(Point(x, y, i));
+		}
+
+		vector<Point> hullVector;
+		std::vector<int> ansIndexes;
+
+		hullVector = convex_hull(points, n);
+
+		// cout << "convex_hull returned\n" ;
+		int hullVectorSize = hullVector.size();
+
+		if(hullVectorSize == 0){
+			printf("%.2lf\n1\n\n", 0.00);
+
+		}
+		else{
+			double distance = hull_perimeter(hullVector, hullVectorSize);
+			// cout << "distnace calculated\n"  ;
+			printf("%.2lf\n", distance);
+			for(int i = 0 ; i < hullVectorSize ; i++){
+				cout << hullVector[i].pos+1 << " " ;
+			}
+			cout << "\n\n";
+		}
 	}
-
-	vector<Point> hullVector;
-	std::vector<int> ansIndexes;
-
-	hullVector = convex_hull(points, n);
-
-	int hullVectorSize = hullVector.size();
-
-	// // Store the indexes of the hull in order
-	// for(int i = 0 ; i< n ;i++){
-	// 	ansIndexes.push_back(hullVector[i].pos) ;
-	// }
-
-	// sort(hullVector.begin(), hullVector.end(), compare);
-	// print(hullVector);
-	double distance = hull_perimeter(hullVector, hullVectorSize);
-	printf("Distance = %.2lf\n", distance);
 
 	return 0 ;
 }
