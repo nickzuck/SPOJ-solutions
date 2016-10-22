d4f562ee17fbbcae2b22130a450f3f43e3a6149f
nickzuck <nikhil1945.singh@gmail.com>
2016-10-22 15:01:07 +0530

    WA again :/

--- scubadiv.cpp
+++ scubadiv.cpp
@@ -4,42 +4,44 @@ using namespace std ;
 
 int main()
 {
-    int t, no, nt ;
-    cin >>t ;
-    while(t--){
-        cin >> no >> nt ;
-        int N ;
-        cin >> N ;
-        int oxy[N], nitro[N], wt[N];
+    int c ;
+    cin >> c;
+    while(c--){
+        int t , a ; 
+        cin >> t >> a ;
 
-        for(int i = 0 ; i < N ;i++){
-            cin >> oxy[i] >> nitro[i] >> wt[i];
+        int n ; 
+        cin >> n;
+
+        int oxygen[n+1], nitrogen[n+1], weight[n+1]; 
+        for(int i = 0 ;i < n ;i++){
+            cin >> oxygen[i] >> nitrogen[i] >> weight[i];
         }
-        
-        long  int dp[N+1][no+1][nt+1];
-        for(int i = 0 ;i <=N ;i++){
-            for(int j = 0 ; j <= no ; j++){
-                for(int k = 0; k <= nt; k++){
-                    if(i == 0){
-                        dp[i][j][k] = INT_MAX ;
-                        if(j == 0 and k == 0){
-                            dp[i][j][k] = 0;
-                        }
-                    }
 
-                   else{/*
-                    cout << i  <<"  "<< j << "  " << k << endl;
-                    cout << "dp[i-1][j][k] = " << dp[i-1][j][k] << endl;
-                    cout << " wt[i] + dp[i-1][max(0,j- oxy[i])][max(0, k-nitro[i])] = " <<  wt[i] + dp[i-1][max(0,j- oxy[i])][max(0, k-nitro[i])] << endl;
-                    cout << "max(0,j- oxy[i]) = " << max(0,j- oxy[i]) << endl;
-                    cout << "max(0, k-nitro[i])  = " << max(0, k-nitro[i]) << endl; 
-                    cout << "dp[i-1][max(0,j- oxy[i])][max(0, k-nitro[i])] = " << dp[i-1][max(0,j- oxy[i])][max(0, k-nitro[i])] << endl; */
-                    dp[i][j][k] = min(dp[i-1][j][k] , wt[i] + dp[i-1][max(0,j- oxy[i])][max(0, k-nitro[i])]);
-                   }
+        // Processing
+        int dp[2][t+1][a+1];
+        for(int i = 0 ;i < t ; i++){
+            for(int j = 0 ; j < a ; j++){
+                dp[1][i][j] = 800000;
+            }
+        }
+        dp[1][0][0] = 0 ;
+        
+        
+        int t1, t2 ;
+        for(int i = 0, r = 0 ; i < n ; i++, r ^=1){
+            for(int j = 0 ; j <= t ; j++){
+                for(int k = 0 ; k <= a ; k++){
+                    t1 = max(0 , j - oxygen[i]);
+                    t2 = max(0 , k - nitrogen[i]);
+                    dp[r][j][k] = min(dp[r^1][j][k], dp[r^1][t1][t2] + weight[i]);
                 }
             }
         }
-        cout << dp[N][no][nt] << endl;
+
+        cout << dp[(n-1)&1][t][a] << endl;
+        
+
     }
 return 0 ;
 }
