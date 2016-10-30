7e548b52872da4066935b8239366d28ccfe07b39
nickzuck <nikhil1945.singh@gmail.com>
2016-10-30 13:52:25 +0530

    AC.. Fully my efforts.. no hint from outside.. #happy

--- buglife.cpp
+++ buglife.cpp
@@ -1,9 +1,8 @@
-#include<iostream>
-#include<vector>
-#include<stack>
+#include<bits/stdc++.h>
 
 using namespace std ;
 
+/*
 int checkGraph(vector<int> adj[], int nodes){
     const int size = nodes ;
 
@@ -40,8 +39,53 @@ int checkGraph(vector<int> adj[], int nodes){
         }
     }
     return 1 ;
+}*/
+
+// Checks if the genders are opposite, if yes then return 1,else 0
+int dfs(vector<int> adj[], int n , int visited[], int node){
+    stack<pair<int, int> > s ;
+    pair<int, int> tempPair;
+    if(visited[node] == -1){
+        s.push(make_pair(node, 0));
+    }
+
+    else{
+        return 1 ; // return true if the node is already visited
+    }
+    while(!s.empty()){
+        tempPair = s.top() ; s.pop() ;
+        for(int i = 0 ; i<adj[tempPair.first].size() ; i++){
+            // If node is already visited check for color
+            // else assign it a color
+            if(visited[adj[tempPair.first][i]] == -1) {
+                s.push(make_pair(adj[tempPair.first][i], !tempPair.second));
+                visited[adj[tempPair.first][i]] = !tempPair.second ;
+            }
+            else{
+                if(visited[adj[tempPair.first][i]] == tempPair.second){
+                    // Seomthing fisshy ;
+                    return 0 ;
+                }
+            }
+        }
+    }
+    // If the dfs for the nodes passes good, then everything is good 
+    // till now as expected
+    return 1 ;
 }
 
+int isCorrect(vector<int> adj[], int n){
+    int visited[n];
+    memset(visited, -1, sizeof(visited));
+    int tempAns ; 
+    for(int i = 0 ; i < n; i++){
+        tempAns = dfs(adj,n, visited, i);
+        if(tempAns == 0)
+            return 0;
+    }
+// If all the dfs gives answer correct, then return 1
+return 1 ;
+}
 int main()
 {
     int t,i , nodes, edges, x , y , j , ans ;
@@ -51,10 +95,10 @@ int main()
         cin >> nodes >> edges ; 
         for(j = 0 ; j < edges ; j++){
             cin >> x >> y ; 
-            adj[x].push_back(y) ; 
-            adj[y].push_back(x) ;
+            adj[x-1].push_back(y-1) ; 
+            adj[y-1].push_back(x-1) ;
         }
-        ans = checkGraph(adj , nodes) ;
+        ans = isCorrect(adj , nodes) ;
 
         cout << "Scenario #" << i+1  << ":"<< endl  ;
         if(ans){
