b88acfd08f247763688f5886717aafc80352f4aa
nickzuck <nikhil1945.singh@gmail.com>
2016-11-01 11:59:50 +0530

    WA

---
+++ mcur98.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+#define MAX 1000002
+using namespace std; 
+
+int a[MAX] = {0};
+
+int sodFunc(int n){
+    int sum = 0 ; 
+    while(n!= 0){
+        sum += n%10 ;
+        n /= 10;
+    }
+
+    return sum ;
+}
+
+int main()
+{
+    int sod ; 
+    for(int i = 1; i<=MAX-2; i++){
+        if(a[i] == 0)
+            printf("%d\n", i);
+        sod = sodFunc(i);
+        if(sod + i < MAX){
+            a[sod+i] = 1 ;
+        }
+    }
+return 0 ;
+}
