7b370dd763f41719e53f5399b03e06de1c63fc9c
nickzuck <nikhil1945.singh@gmail.com>
2017-01-02 23:25:55 +0530

    Finally AC...after so much of efforts in not finding the bugs

--- lks.cpp
+++ lks.cpp
@@ -1,65 +1,68 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 
-#define MAX 10002
-// #define int long long
-// #define void int 
-#define ll long long
-using namespace std ;
+#define MAX 501
+using namespace std; 
 
-
-ll  knapsack (ll value[] , ll wt[] , ll n , ll maxW);
-
-ll knapsack (ll value[] , ll wt[] , ll n , ll maxW)
-{
-
-    ll j , w , knap[2][maxW+1] ;
-    /*for (i = 0 ; i< n ; i++)
-    {
-        cout << value[i] << " " << wt[i] << endl ;
-    }*/
-    
-    // Not needed as it can be done in one for loop
-    /*
-    for (j = 0 ; j<=n ; j++){
-        knap[0][j] = 0 ;        
+int knap[2][2000002] = {0};
+void print(int val[], int weight[], int n , int maxW){
+    for(int i = 0 ; i <= maxW; i++){
+        cout << knap[0][i] << " " ;
     }
-    */
-
-    for (j = 0 ; j<=maxW ; j++){
-        knap[1][j] = 0 ; 
-        knap[0][j] = 0 ;
+    cout << endl ;
+    for(int i = 0 ; i <= maxW; i++){
+        cout << knap[1][i] << " " ;
     }
-    bool storeModulo;
-    for (j = 1 ; j<=n ; j++) {
-        for (w = 1 ; w <= maxW ; w++) {
-            storeModulo = j%2 ;
-            if (wt[j-1] > w) { // we use j-1 because  the array contains the value from the 0 index
-                knap[storeModulo][w] = knap[storeModulo?0:1][w-1]; /* the current weight of the item is greater than the size the knapsack can hold*/
+    cout << endl ;
+}
+int knapsack(int val[], int weight[], int n , int maxW){
+    bool a  ;
+
+    //Testing 
+    // for(int i = 0 ; i <= maxW; i++){
+    //     cout << knap[0][i] << " " ;
+    // }
+    // cout << endl ;
+    // for(int i = 0 ; i <= maxW; i++){
+    //     cout << knap[1][i] << " " ;
+    // }
+    // cout << endl ;
+    //Testing ends here
+
+    for(int i = 1 ; i <=n ;i ++){
+        a = i%2 ;
+        for(int j = 1 ; j <= maxW ; j ++){
+            if(weight[i-1] <= j){
+                knap[a][j] = max( knap[!a][j- weight[i-1]] + val[i-1], 
+                                  knap[!a][j]
+                                ) ;
             }
-
-            else {
-                knap[storeModulo][w] = max(knap[storeModulo?0:1][w-wt[j-1]] +value[j-1] , knap[storeModulo?0:1][w]) ;  /* either the item j is needed to achieve the max value or it's not needed*/
+            else{
+                knap[a][j] = knap[!a][j];
             }
         }
+        //print(val, weight, n, maxW );
     }
-
-return max(knap[0][maxW], knap[1][maxW]);
+    //cout << knap[1][maxW] << " " << knap[0][maxW]  << endl;
+    if(n&1){
+        return knap[1][maxW];
+    }
+    else
+        return knap[0][maxW];
 }
 
-
-int main ()
-{
-    ll n , maxW ;  //no of bags and the maximum weight the knapsack can hold 
-  //  cout << "Enter the n and Max Weight " ; 
-    cin  >> maxW >> n  ; 
-    ll v[n+1] , w[n+1] , i , j; // v = value of each item , w = weight of each item 
-   // cout << "Enter the weight and the corresponding value of each item\n" ; 
-    for (i = 0 ; i<n ; i++){
-        cin >> v[i] >> w[i] ; 
-    }
+int main(){
+    int n, maxW ; 
+    int val[MAX], weight[MAX] ;
     
-    cout << knapsack(v , w , n , maxW) << endl;
+    //Input
+    cin >> maxW >> n ; 
+    for(int i = 0 ;i < n ; i++){
+        cin >> val[i] >> weight[i];
+    }
 
-return 0 ; 
+    //Processing
+    int ans ; 
+    ans = knapsack(val, weight, n , maxW);
+    cout << ans << endl;
+    return 0 ;
 }
