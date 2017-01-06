088f849640942a799e0281ad6dd1cc0b2bbbed80
nickzuck <nikhil1945.singh@gmail.com>
2017-01-06 16:08:22 +0530

    Removed the silly mistakes ... still getting a WA

--- diehard.cpp
+++ diehard.cpp
@@ -44,18 +44,15 @@ int main()
         cin >> h >> a ;
         //cout << "After input\n";
         int a1 = solve(0, h+ha[0], a+aa[0]) ;
-        if(a1 != 0)
-            dp[t1][t2] = a1 ; 
+        
         //cout << "After a1 assignment\n";
         int a2 = solve(1, h+ha[1], a+aa[1]) ;
         //cout << "After a2\n";
-        if(a2 != 0)
-            dp[h+ha[1]][a+aa[1]]  = a2 ;
+        
         //cout << "After a2 assignment \n";
         int a3 = solve(2, h+ha[2], a+aa[2]) ;
         //cout << "After a3\n";
-        if(a3 != 0)
-            dp[h+ha[2]][a+aa[2]]  = a3 ;
+        
         //cout << "After a3 assignment\n";
         //cout << a1 << a2 << a3 << endl;
         cout << max(a1, max(a2, a3)) << endl;
