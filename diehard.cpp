0ee71c80c8a876aabd4c6f8cb7774fb15f368916
nickzuck <nikhil1945.singh@gmail.com>
2017-01-06 21:34:12 +0530

    Finally solved it
    How could I be sol much foolish to have so many WA's for such an easy DP

--- diehard.cpp
+++ diehard.cpp
@@ -9,28 +9,22 @@ using namespace std ;
     int aa[] = {5, -10, 2};
 
 
-        static int dp[MAX][MAX];
+    int dp[MAX][MAX];
 int solve(int curr, int h, int a){
    // cout << "h = " << h << "  a = " << a << endl;
     if(h <= 0 || a <= 0)
         return 0 ;
     //cout << "Before dp check\n";
-    if(dp[h][a] != 0){
+    if(dp[h][a] != -1){
     //    cout << "In dp check\n";
         return dp[h][a];
     }
-    switch(curr){
-        case 0: //cout << "h = ";
-                dp[h][a] =  (1 + max(solve(1, h+ha[1], a+aa[1]), solve(2, h+ha[2], a+aa[2])));
-                return dp[h][a];
-        case 1: //cout << "In case 1 \n" ;
-
-                 dp[h][a] = (1 + max(solve(0, h+ha[0], a+aa[0]), solve(2, h+ha[2], a+aa[2])));
-                 return dp[h][a];
-        case 2: //cout << "In case 2\n";
-                dp[h][a] = (1 + max(solve(0, h+ha[0], a+aa[0]), solve(1, h+ha[1], a+aa[1])));
-                return dp[h][a];
+    // If it is air check for the possible solution with water and air
+    if(curr != 2){
+        dp[h][a] = max(dp[h][a], 1+solve(2, h+ha[2], a+aa[2]));
     }
+    dp[h][a] = max(dp[h][a], max(1+solve(1, h+ha[1], a+aa[1]), 1+solve(0, h+ha[0], a+aa[0])));
+    return dp[h][a];
 }
 
 int main()
@@ -38,24 +32,15 @@ int main()
     int t ; 
     cin >> t; 
     while(t--){
-        //memset(dp, sizeof(dp), -1);
+        memset(dp, -1, sizeof(dp));
         int h , a, t1 , t2;
-        //cout << "Before input\n";
+
         cin >> h >> a ;
-        //cout << "After input\n";
-        int a1 = solve(0, h+ha[0], a+aa[0]) ;
-        
-        //cout << "After a1 assignment\n";
-        int a2 = solve(1, h+ha[1], a+aa[1]) ;
-        //cout << "After a2\n";
-        
-        //cout << "After a2 assignment \n";
-        int a3 = solve(2, h+ha[2], a+aa[2]) ;
-        //cout << "After a3\n";
-        
-        //cout << "After a3 assignment\n";
-        //cout << a1 << a2 << a3 << endl;
-        cout << max(a1, max(a2, a3)) << endl;
+
+        // Always start with air as it gives you both
+        // health and armor
+        cout << solve(2, h+ha[2], a+aa[2]) << endl;
+        //cout << max(a1, max(a2, a3)) << endl;
     }
 return 0 ;
 }
