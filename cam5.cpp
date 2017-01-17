8789a2e315d7799f48c0e28035551f42f453d0a1
nickzuck <nikhil1945.singh@gmail.com>
2017-01-18 00:10:37 +0530

    Getting Segmentation fault

---
+++ cam5.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+#define MAX 100002
+#define mp make_pair
+#define pb push_back
+using namespace std ;
+
+bool visited[MAX];
+
+void initialize(int n){
+    for(int i = 0 ; i < n ; i++){
+        visited[i] = false ;
+    }
+}
+
+void dfs(int source, vector<int> graph[]){
+    stack<int> s;
+    s.push(source);
+    while(!s.empty()){
+        int item = s.top() ;
+        s.pop();
+        visited[item] = true;
+        for(int i = 0 ;i <graph[item].size() ; i++){
+            int temp = graph[item][i];
+            if(!visited[temp])
+                s.push(graph[item][i]);
+        }
+    }
+}
+
+void pa(int n){
+    for(int i = 0 ; i < n ; i++)
+        cout << visited[i] << " " ;
+    cout << endl ;
+}
+int main()
+{
+    int t, n ;
+    cin >> t ; 
+    while(t--){
+        cin >> n ;
+        vector<int> graph[n];
+        int e, a, b ;
+        cin >> e ;
+        for(int i = 0 ;i < e; i++){
+            cin >> a >> b;
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
+        
+        initialize(n);
+        // Processing
+        int count = 0;
+        for(int i = 0 ;i < n ; i++){
+            if(!visited[i]){
+                dfs(i, graph) ;
+                count ++ ;
+            }
+        }
+        cout << count << endl ;
+        
+    }
+return 0;
+}
